7d8814266ab0950fba33aec0b7119e3952772e61
Cringetiaan <chris123robaard@gmail.com>
2023-04-11 13:16:40 +0200

    Hunger Bar works actually this time

--- harryhammie/Assets/Scripts/UI scripts/hungerControl.cs
+++ harryhammie/Assets/Scripts/UI scripts/hungerControl.cs
@@ -5,13 +5,16 @@ using UnityEngine.UI;
 
 public class hungerControl : MonoBehaviour
 {
+
+    public GameObject PlsFix;
     // 9/4/23- Ren H: setting up to change on btn click
     int feed = 0;
     public Slider slider;
-
+    
+    
     public void UpdateFeed(){
         feed++;
-        slider.value = feed;
+        PlsFix.GetComponent<HungerBar>().gameHourTime = PlsFix.GetComponent<HungerBar>().gameHourTime + feed;
     }
 
 }
