aee5da1dd676ca0512b3b511a7d4ff827bc1c546
GtafanWRLD <mispanda5555@wp.pl>
2023-06-08 10:48:22 +0200

    Play button fix
    Play now runs level 1, attempt to fix the aspect ratio

--- harryhammie/Assets/ResolutionChanger.cs
+++ harryhammie/Assets/ResolutionChanger.cs
@@ -2,16 +2,13 @@ using UnityEngine;
 
 public class ResolutionChanger : MonoBehaviour
 {
-    public int screenWidth = 1920;  // New screen width
-    public int screenHeight = 1080; // New screen height
-
-    void Start()
-    {
-        ChangeResolution();
-    }
-
-    public void ChangeResolution()
-    {
-        Screen.SetResolution(screenWidth, screenHeight, Screen.fullScreen);
-    }
+    // Use this for initialization
+ void Start () {
+ Screen.orientation = ScreenOrientation.LandscapeLeft;
+ }
+ 
+ // Update is called once per frame
+ void Update () {
+ 
+ }
 }
